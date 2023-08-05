faefa8210f15d64e484fbd4f586d3141cdfe0ca1
GamerChris0837 <jjangga4227@gmail.com>
2023-08-05 15:00:35 +0900

    :memo:update

--- io/2.2.py
+++ io/2.2.py
@@ -12,7 +12,14 @@ print(3/5)
 a=1/3
 print(a)
 print("%.1f" % a)
+# %s는 'String'의 첫 글자로서 문자열을 의미함.
+# %d는 'Digit'의 첫 글자로 정수형 숫자를 의미함.
+# %f는 'Floating Point'의 첫 글자로서 실수형 숫자를 의미함.
 
+
+# type() 함수로 특정 변수가 정수인지 실수인지 알 수 있다.
+# int는 'Integer'의 약자로 정수를 의미
+# float는 'Floating Point'의 약자로 실수를 의미
 a=200
 print(type(a))
 
@@ -30,6 +37,7 @@ print(d)
 print(type(c))
 print(type(d))
 
+# %은 나머지 연산자(어떤 수를 나눈 나머지)
 a=10%3
 print(a)
 
@@ -40,7 +48,10 @@ c=a%b
 print(c)
 
 print(15/4)
+
+# //는 소수점 절삭 연산자(소수점 표시 안함)
 print(15//4)
 
+# **은 거듭제곱 연산자
 print(3**3)
 print(10**4)
