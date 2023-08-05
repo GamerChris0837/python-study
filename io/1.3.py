fba4d2c80ca970ef290f538167131c9927834992
woo1218 <ilovesunkr@gmail.com>
2023-08-05 11:33:53 +0900

    :memo:update

---
+++ io/1.3.py
@@ -0,0 +1,26 @@
+print(3 + 5)
+print(10 - 20)
+print(5*8)
+print(10/2)
+print(100 + 10*3)
+print((100 + 10)*3)
+
+print("ㅎㅇㅇ")
+
+print("정종혁")
+print("kinjeok")
+print("010-8405-0612")
+
+a = 15
+b = 35
+c = a + b
+
+print(c)
+
+num1 = 100
+num2 = 50
+result = num1*num2
+print(result)
+
+print(5+3*(15))
+
