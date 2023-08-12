c99cd4bf61622d7afb630b1329210513ce624bfe
GamerChris0837 <jjangga4227@gmail.com>
2023-08-12 15:19:08 +0900

    :memo:update

---
+++ 연습.py
@@ -0,0 +1,13 @@
+id=input("아이디?")
+password=input("비번?")
+
+print("아이디 :",id)
+print("비밀번호 :",password)
+a=int(input("걍 아무 번호 누르셈"))
+
+if a==1:
+    print("1")
+elif a==2:
+    print("22")
+else:
+    print(":)")
