bbed82aaa5b36522073825e0611ff64789d9f651
GamerChris0837 <jjangga4227@gmail.com>
2023-08-13 11:46:11 +0900

    :memo:update

---
+++ hello.py
@@ -0,0 +1,5 @@
+def greet1(name):
+    print(name+"야 안녕")
+
+def greet2(name):
+    print(name+"님 안녕하세유")
--- 연습5.py
+++ 연습5.py
@@ -44,3 +44,56 @@ def sum_int(start,end):
     print("%d ~ %d 정수 합계 : %d"% (start,end,total))
 
 sum_int(start,end)
+
+# 선형 탐색으로 최솟값 찾기
+def find_min(n):
+    smallest=n[0]
+    for i in range(1,len(n)):
+        if n[i]<smallest:
+            smallest=n[i]
+
+    return smallest
+
+data=[6,3,-2,12,5,-3,17,9,13,16]
+print(data)
+
+min_value=find_min(data)
+print('최솟값 :',min_value)
+
+# 이진 탐색으로 내림차순에서 특정 값을 찾기
+def binary_search(n,x):
+    start=0
+    end=len(n)-1
+
+    while start<=end:
+        mid=(start+end)//2
+        if x==n[mid]:
+            return mid
+        elif x>n[mid]:
+            end=mid-1
+        else:
+            start=mid+1
+
+    return -1
+
+data=[93,91,89,87,80,61,55,43,41,38,32,30,25,20,8,2]
+print(data)
+
+search_num=int(input("찾는 수를 적으세요 : "))
+index=binary_search(data,search_num)
+print('%d의 인덱스 번호 : %d'% (search_num,index))
+
+# 짝수 홀수 판별하기
+
+x=int(input("양의 정수를 입력하세요 : "))
+
+if x%2==0:
+    print("짝수!")
+else:
+    print("홀수!")
+
+# 모듈
+import hello
+
+hello.greet1("chris")
+hello.greet2("GChr1s")
